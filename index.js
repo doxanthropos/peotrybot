c9105a4016d4401f806b301ed53293517bc0c17a
Knut Jackowski <doxanthropos@googlemail.com>
2018-10-12 22:36:48 +0200

    Tooting ca. once an hour.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Mastodon = require('mastodon-api');
 const tokens = require('./tokens.js');
-
+var message;
 console.log("Bot starting!");
 
 const M = new Mastodon({
@@ -10,16 +10,30 @@ const M = new Mastodon({
   timeout_ms: 60 * 1000,
   api_url: 'https://botsin.space/api/v1/',
 })
+function toot(){
+  const meaning = 42;
+  const guess = Math.floor(Math.random() * 100);
+  if(guess === meaning){
+    message = `Is the meaning of life ${guess}? Yes it is.`;
+    clearInterval(intervalID);
+  } else {
+    message = `Is the meaning of life ${guess}? No. Sadly not.`;
+  }
+  const params = {
+    status: message
+  }
 
-const params = {
-  status: "Hello World!"
+  M.post('statuses', params, (error, data) => {
+    if(error){
+      console.error(error);
+    } else {
+      console.log(`ID: ${data.id} \nTimestamp: ${data.created_at}`);
+      console.log(data.content);
+    }
+  });
 }
 
-M.post('statuses', params, (error, data) => {
-  if(error){
-    console.error(error);
-  } else {
-    console.log(data);
-  }
-});
+toot();
+const intervalID = setInterval(toot, 60 * 60 * 1000);
+
 
